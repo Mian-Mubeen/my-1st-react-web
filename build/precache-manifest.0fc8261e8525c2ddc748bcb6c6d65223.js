c873ffac597695835a981de487ff630b3932cf9b
Mian-Mubeen <108216172+Mian-Mubeen@users.noreply.github.com>
2022-09-09 23:32:27 +0500

    Add files via upload

---
+++ build/precache-manifest.0fc8261e8525c2ddc748bcb6c6d65223.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "45bc02bf915fc4773f3f",
+    "url": "/my-1st-react-web/static/css/main.657f3f98.chunk.css"
+  },
+  {
+    "revision": "45bc02bf915fc4773f3f",
+    "url": "/my-1st-react-web/static/js/main.45bc02bf.chunk.js"
+  },
+  {
+    "revision": "c972895fac6c9db70f84",
+    "url": "/my-1st-react-web/static/js/1.c972895f.chunk.js"
+  },
+  {
+    "revision": "5cfab13915b2c349a988",
+    "url": "/my-1st-react-web/static/js/2.5cfab139.chunk.js"
+  },
+  {
+    "revision": "d99c87fc01c1743a9b3d",
+    "url": "/my-1st-react-web/static/js/runtime~main.d99c87fc.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/my-1st-react-web/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "1d7a512dd06b1de58198e2fa835b58ed",
+    "url": "/my-1st-react-web/index.html"
+  }
+];
